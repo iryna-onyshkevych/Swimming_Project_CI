4238c7ee5961d1a2ff0d078af7e90c2256eed8bb
Iryna Onyshkevych <onyshkevych.iryna@gmail.com>
2021-03-10 10:25:50 +0200

    Some changes

--- Swimming.ADO.DAL/Connection/Connection.cs
+++ Swimming.ADO.DAL/Connection/Connection.cs
@@ -7,7 +7,7 @@ namespace Swimming.ADO.DAL.Repositories.Connection
         //private readonly string connection = @"Data Source=.\SQLEXPRESS;Initial Catalog=swimming;Integrated Security=True";
         private readonly string connection = @"Server = tcp:irynaonyshkevych1.database.windows.net,1433;Initial Catalog = swimming; Persist Security Info=False;User ID = login1; Password=Ilikedg38; MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout = 30";
 
-
+        //test comment
         public SqlConnection CreateSqlConnection()
         {
             SqlConnection sqlConnection = new SqlConnection(connection);
